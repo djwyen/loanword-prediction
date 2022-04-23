92193372277f90e58050fba3912b8c11b977a0ee
djwyen <djwyen@mit.edu>
2022-04-23 01:07:21 -0400

    Add tests for Dataset

--- dataset_test.py
+++ dataset_test.py
@@ -47,30 +47,33 @@ class TestWord(unittest.TestCase):
         self.assertEqual(pretty_segs[0][0], 's')
         self.assertEqual(pretty_segs[4][1], a_features)
 
-# class TestDataset(unittest.TestCase):
-#     def setUp(self):
-#         self.dataset = BCCWJDataset()
+class TestDataset(unittest.TestCase):
+    def setUp(self):
+        self.dataset = BCCWJDataset()
 
-#     def test_size(self):
-#         self.assertEqual(len(self.dataset), OUTPUT_CSV_LENGTH)
+    def test_size(self):
+        self.assertEqual(len(self.dataset), OUTPUT_CSV_LENGTH)
 
-#     def test_accesses(self):
-#         # TODO wait for BCCWJDataset to change its __getitem__ to return Words before filling this out
-#         first_access = self.dataset[0]
-#         # should be だ,ダ,da,和
-#         self.assertEqual(first_access)
+    def test_accesses(self):
+        first_access = self.dataset[0]
+        # should be だ,ダ,da,和
+        self.assertEqual(str(first_access), '0: だ, ダ, da')
+        self.assertEqual(first_access.origin, '和')
 
-#         last_access = self.dataset[OUTPUT_CSV_LENGTH - 1]
-#         # should be ワーピア,ワーピア,ɰaːpia,固
+        last_access = self.dataset[OUTPUT_CSV_LENGTH - 1]
+        # should be ワーピア,ワーピア,ɰaːpia,固
+        self.assertEqual(str(last_access), '36395: ワーピア, ワーピア, ɰaːpia')
+        self.assertEqual(last_access.origin, '固')
 
-#         middle_access_1 = self.dataset[17512]
-#         # should be 撃沈,ゲキチン,ɡekit͡ɕiɴ,漢
+        middle_access_1 = self.dataset[17511]
+        # should be 撃沈,ゲキチン,ɡekit͡ɕiɴ,漢
+        self.assertEqual(str(middle_access_1), '17511: 撃沈, ゲキチン, ɡekit͡ɕiɴ')
+        self.assertEqual(middle_access_1.origin, '漢')
 
-#         middle_access_2 = self.dataset[8928]
-#         # should be ノック,ノック,nokkɯ,外
-
-
-
+        middle_access_2 = self.dataset[8928]
+        # should be ノック,ノック,nokkɯ,外
+        self.assertEqual(str(middle_access_2), '8928: ノック, ノック, nokkɯ')
+        self.assertEqual(middle_access_2.origin, '外')
 
 
 if __name__ == '__main__':
