9c59bcc212721aefac34a72ca5a96764cb34887e
djwyen <djwyen@mit.edu>
2022-04-24 21:49:47 -0400

    Refactor Word again for Dataset computation efficiency

--- data_processing/bccwj_dataset.py
+++ data_processing/bccwj_dataset.py
@@ -8,6 +8,7 @@ import os
 import csv
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
+from .transcriber import Transcriber
 from .word import Word
 
 # path to the pared BCCWJ dataset that is created by `process_bccwj.py`
@@ -16,6 +17,7 @@ PATH_TO_PROCESSED_CSV = "data/BCCWJ/pared_BCCWJ.csv"
 class BCCWJDataset(Dataset):
     def __init__(self):
         self.vocab_df = pd.read_csv(PATH_TO_PROCESSED_CSV)
+        self._t = Transcriber()
 
     def __len__(self):
         return self.vocab_df.shape[0]
@@ -25,4 +27,6 @@ class BCCWJDataset(Dataset):
         kana = self.vocab_df.at[idx, 'kana']
         ipa = self.vocab_df.at[idx, 'ipa']
         origin = self.vocab_df.at[idx, 'origin']
-        return Word(idx, word, kana, origin, ipa)
+        segments = self._t.ipa_to_panphon_word(ipa)
+        feature_vectors = self._t.ipa_to_numpy_array(ipa)
+        return Word(idx, word, kana, origin, ipa, segments, feature_vectors)
--- data_processing/word.py
+++ data_processing/word.py
@@ -4,25 +4,18 @@ namely its original idx in the pared dataset, its lemma (word), its form in kana
 its sublexical origin, its transcription in IPA, and the panphon segments it induces
 """
 
-from .transcriber import Transcriber
-
 class Word():
     # TODO add another form of the segments besides just the vectors
     #      and in particular make it possible to access things by particular features like voicing or whatever
     #      the upshot is, read the panphon documentation...
-    def __init__(self, idx, word, kana, origin, ipa):
-        self._t = Transcriber()
+    def __init__(self, idx, word, kana, origin, ipa, segments, feature_vectors):
         self.idx = idx # note that a word's ID will not necessarily be its key in the BCCWJDataset due to shuffling
         self.word = word
         self.kana = kana
         self.origin = origin
         self.ipa = ipa
+        self.segments = segments
+        self.feature_vectors = feature_vectors
     
     def __str__(self):
         return f'{self.idx}: {self.word}, {self.kana}, {self.ipa}'
-    
-    def segments(self):
-        return self._t.ipa_to_panphon_word(self.ipa)
-    
-    def vectors(self):
-        return self._t.ipa_to_numpy_array(self.ipa)
