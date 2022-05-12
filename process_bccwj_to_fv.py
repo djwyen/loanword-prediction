59f906df4ad0c48991ae0e6c3efa8a8447650365
djwyen <djwyen@mit.edu>
2022-05-12 18:21:01 -0400

    Process to also save word lengths before padding

--- process_bccwj_to_fv.py
+++ process_bccwj_to_fv.py
@@ -10,6 +10,7 @@ from utils import segment_len_of_ipa
 
 PATH_TO_PROCESSED_CSV = "data/BCCWJ/pared_BCCWJ.csv"
 PATH_TO_FV_GZ = "data/BCCWJ/fv_pared_BCCWJ.gz"
+PATH_TO_LENGTHS_GZ = "data/BCCWJ/BCCWJ_lengths.gz"
 
 MAX_SEQ_LEN_NO_PAD = 20
 NUM_PHONETIC_FEATURES = 22 # Panphon by default gives you 24 features, but the last two corresond to tonal features so I drop them
@@ -20,6 +21,7 @@ PAD_BINARY_FV = [1] * NUM_PHONETIC_FEATURES
 def main():
     # the huge array that will store a representation of each word
     word_array = []
+    lengths_array = [] # the array containing the lengths of each word, in segments
 
     t = Transcriber()
     with open(PATH_TO_PROCESSED_CSV) as f:
@@ -32,8 +34,10 @@ def main():
             fv = t.shorthand_to_fv(shorthand)
             fv = t.fv_to_binary_fv(fv)
 
+            word_length = segment_len_of_ipa(ipa)
+
             # add the end-of-word token and then pad to the max seq length
-            length_diff = MAX_SEQ_LEN_NO_PAD - segment_len_of_ipa(ipa)
+            length_diff = MAX_SEQ_LEN_NO_PAD - word_length
             fv.append(END_BINARY_FV)
             for _ in range(length_diff):
                 fv.append(PAD_BINARY_FV)
@@ -44,9 +48,12 @@ def main():
                 flat_fv.extend(vec)
             
             word_array.append(flat_fv)
+            lengths_array.append(word_length)
     
     word_array = np.array(word_array)
+    lengths_array = np.array(lengths_array)
     np.savetxt(PATH_TO_FV_GZ, word_array)
+    np.savetxt(PATH_TO_LENGTHS_GZ, lengths_array)
 
 if __name__ == '__main__':
     main()
